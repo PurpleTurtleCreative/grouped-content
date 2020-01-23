5ca63cd92aa4ac154f3b8a33f009e7552ee25968
Michelle Blanchette <michelle@michellegraphics.com>
2020-01-23 17:05:17 -0500

    FIX: Would stop JavaScript execution with Classic Editor due to undefined function call

--- assets/js/metabox-page-relatives.js
+++ assets/js/metabox-page-relatives.js
@@ -1,73 +1,77 @@
 jQuery(function($) {
 
-  var hasChangedParentPage = false;//keeps track if parent page was actually changed between multiple saves
-  var requestRefreshTimers = [];//keep track of timers for clearing once successful
+  if ( wp.data ) {
 
-  wp.data.subscribe(function() {
+    var hasChangedParentPage = false;//keeps track if parent page was actually changed between multiple saves
+    var requestRefreshTimers = [];//keep track of timers for clearing once successful
 
-    var isSavingPost = wp.data.select('core/editor').isSavingPost();//boolean
-    var isAutosavingPost = wp.data.select('core/editor').isAutosavingPost();//boolean
-    var hadSuccessfulSave = wp.data.select('core/editor').didPostSaveRequestSucceed();//boolean
+    wp.data.subscribe(function() {
 
-    var currentParentPage = wp.data.select( 'core/editor' ).getCurrentPostAttribute( 'parent' );//int
-    var editedParentPage = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'parent' );//string
+      var isSavingPost = wp.data.select('core/editor').isSavingPost();//boolean
+      var isAutosavingPost = wp.data.select('core/editor').isAutosavingPost();//boolean
+      var hadSuccessfulSave = wp.data.select('core/editor').didPostSaveRequestSucceed();//boolean
 
-    if (
-      isSavingPost
-      && ! isAutosavingPost
-      && editedParentPage == currentParentPage
-      && hadSuccessfulSave
-      && hasChangedParentPage
-    ) {
+      var currentParentPage = wp.data.select( 'core/editor' ).getCurrentPostAttribute( 'parent' );//int
+      var editedParentPage = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'parent' );//string
 
-      requestRefresh = setTimeout(function() {
+      if (
+        isSavingPost
+        && ! isAutosavingPost
+        && editedParentPage == currentParentPage
+        && hadSuccessfulSave
+        && hasChangedParentPage
+      ) {
 
-        var metaboxContainer = $('#ptc-grouped-content div.inside');
+        requestRefresh = setTimeout(function() {
 
-        var currentHTML = metaboxContainer.html();
-        metaboxContainer.html('<p id="ptc-notice-refreshing-metabox"><i class="fas fa-sync-alt fa-spin"></i>Refreshing page relatives...</p>');
+          var metaboxContainer = $('#ptc-grouped-content div.inside');
 
-        var data = {
-          'action': 'refresh_page_relatives',
-          'nonce': ptc_page_relatives.nonce,
-          'post_id': wp.data.select('core/editor').getCurrentPostId(),
-          'edited_parent': editedParentPage
-        };
+          var currentHTML = metaboxContainer.html();
+          metaboxContainer.html('<p id="ptc-notice-refreshing-metabox"><i class="fas fa-sync-alt fa-spin"></i>Refreshing page relatives...</p>');
 
-        $.post(ajaxurl, data, function(res) {
+          var data = {
+            'action': 'refresh_page_relatives',
+            'nonce': ptc_page_relatives.nonce,
+            'post_id': wp.data.select('core/editor').getCurrentPostId(),
+            'edited_parent': editedParentPage
+          };
 
-          if(res.status == 'success') {
-            //update metabox content
-            metaboxContainer.html(res.data);
-            //cancel future runs after success
-            requestRefreshTimers.forEach(clearTimeout);
-            requestRefreshTimers = [];
-            //reset to check for new change
-            hasChangedParentPage = false;
-          } else {
-            // console.error(res.data);
-            metaboxContainer.html(currentHTML);
-          }
+          $.post(ajaxurl, data, function(res) {
 
-        }, 'json')
-          .fail(function() {
-            console.error('Failed to refresh Page Relatives metabox content.');
-            metaboxContainer.html(currentHTML);
-          });
+            if(res.status == 'success') {
+              //update metabox content
+              metaboxContainer.html(res.data);
+              //cancel future runs after success
+              requestRefreshTimers.forEach(clearTimeout);
+              requestRefreshTimers = [];
+              //reset to check for new change
+              hasChangedParentPage = false;
+            } else {
+              // console.error(res.data);
+              metaboxContainer.html(currentHTML);
+            }
 
-      }, 1000);
+          }, 'json')
+            .fail(function() {
+              console.error('Failed to refresh Page Relatives metabox content.');
+              metaboxContainer.html(currentHTML);
+            });
 
-      requestRefreshTimers.push(requestRefresh);
+        }, 1000);
 
-    } else if(
-      isSavingPost
-      && ! isAutosavingPost
-      && editedParentPage != currentParentPage
-      && hadSuccessfulSave
-    ) {
-      hasChangedParentPage = true;
-    }//end if successfully updated parent page
+        requestRefreshTimers.push(requestRefresh);
 
-  });//end wp.data.subscribe listener
+      } else if(
+        isSavingPost
+        && ! isAutosavingPost
+        && editedParentPage != currentParentPage
+        && hadSuccessfulSave
+      ) {
+        hasChangedParentPage = true;
+      }//end if successfully updated parent page
+
+    });//end wp.data.subscribe listener
+
+  }//end if wp.data Gutenberg
 
 });//end document ready
