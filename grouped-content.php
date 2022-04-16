3adf094a77c4ce3bcba6a60085c76ad8e5da036d
Michelle Blanchette <33374343+MichelleBlanchette@users.noreply.github.com>
2022-04-16 14:50:19 -0400

    Issue #27 - code audit and cleanup (#28)
    * starting code audit, moved code registration to separate file, added namespace constants to main plugin file
    
    * #27 - refactor code for updated organization, update README

--- grouped-content.php
+++ grouped-content.php
@@ -2,14 +2,16 @@
 /**
  * Grouped Content
  *
+ * @package           PTC_Grouped_Content
  * @author            Michelle Blanchette
- * @copyright         2020 Michelle Blanchette
+ * @copyright         2022 Purple Turtle Creative, LLC
  * @license           GPL-3.0-or-later
  *
- * Plugin Name:       Grouped Content – Enhanced Page Hierarchies
+ * @wordpress-plugin
+ * Plugin Name:       Grouped Content
  * Plugin URI:        https://purpleturtlecreative.com/grouped-content/
- * Description:       Enhances the use of page hierarchies by providing easy access to the parent page, sibling pages, and child pages in your admin area.
- * Version:           1.2.3
+ * Description:       Provides easy access and insight into hierarchical posts' parent page, sibling pages, and child pages in your admin area.
+ * Version:           2.0.0
  * Requires PHP:      7.0
  * Requires at least: 4.7.1
  * Author:            Purple Turtle Creative
@@ -32,287 +34,90 @@ You should have received a copy of the GNU General Public License
 along with Grouped Content. If not, see https://www.gnu.org/licenses/gpl-3.0.txt.
 */
 
+namespace PTC_Grouped_Content;
+
 defined( 'ABSPATH' ) || die();
 
-if ( ! class_exists( '\PTC_Grouped_Content' ) ) {
-  /**
-   * Provides helper functions and information relevant to this plugin for use
-   * in the global space.
-   *
-   * @since 1.0.0
-   */
-  class PTC_Grouped_Content {
+/**
+ * The full file path to this plugin's main file.
+ *
+ * @since 2.0.0
+ */
+define( __NAMESPACE__ . '\PLUGIN_FILE', __FILE__ );
 
-    /**
-     * This plugin's basename.
-     *
-     * @since 1.0.0
-     *
-     * @ignore
-     */
-    public $plugin_title;
+/**
+ * The full file path to this plugin's directory ending with a slash.
+ *
+ * @since 2.0.0
+ */
+define( __NAMESPACE__ . '\PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 
-    /**
-     * The full file path to this plugin's directory ending with a slash.
-     *
-     * @since 1.0.0
-     *
-     * @ignore
-     */
-    public $plugin_path;
+/**
+ * This plugin's current version.
+ *
+ * @since 2.0.0
+ */
+define( __NAMESPACE__ . '\PLUGIN_VERSION', get_file_data( __FILE__, [ 'Version' => 'Version' ], 'plugin' )['Version'] ?? '0.0.0' );
 
-    /**
-     * Get the admin url for the relevant Groups details page.
-     *
-     * @since 1.0.0
-     *
-     * @param int $post_parent_id Optional. The post id to use as the group to
-     * be linked. Default 0 for default Groups home directory url.
-     *
-     * @return string The admin url for the provided group. If the provided id
-     * cannot represent a group, the Groups home directory url is returned.
-     */
-    function get_groups_list_admin_url( int $post_parent_id = 0 ) : string {
+/**
+ * This plugin's basename.
+ *
+ * @since 2.0.0
+ */
+define( __NAMESPACE__ . '\PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
-      require_once $this->plugin_path . 'src/class-ptc-content-group.php';
+/**
+ * The full url to this plugin's directory, ending with a slash.
+ *
+ * @since 2.0.0
+ */
+define( __NAMESPACE__ . '\PLUGIN_URL', plugins_url( '/', __FILE__ ) );
 
-      try {
-        $content_group = new \ptc_grouped_content\PTC_Content_Group( $post_parent_id );
-        $url = admin_url( 'admin.php?page=ptc-grouped-content&post_parent=' . $post_parent_id );
-      } catch ( \Exception $e ) {
-        $url = admin_url( 'admin.php?page=ptc-grouped-content' );
-      }
+/* CODE REGISTRATION */
 
-      return $url;
+/**
+ * Requires a class file and calls its static "register" method.
+ *
+ * Class file names and class names must follow WordPress naming conventions.
+ * For example, /path/to/class-my-class.php should contain the declaration of
+ * class My_Class.
+ *
+ * Intentionally does not check if a "register" method exists in the class
+ * or if the class has been properly included. Doing so would hide ACTUAL errors
+ * due to formatting mistakes that should, indeed, be noticed and fixed!
+ *
+ * @link https://developer.wordpress.org/coding-standards/wordpress-coding-standards/php/#naming-conventions
+ *
+ * @param string $file The full class filename.
+ */
+function register_class_from_file( string $file ) {
 
-    }
+	$class_name = str_replace(
+		[ 'class-', '-' ],
+		[ '', '_' ],
+		basename( $file, '.php' )
+	);
+	$class_name = __NAMESPACE__ . '\\' . ucwords( $class_name, '_' );
 
-    /* Plugin Initialization */
+	if ( ! class_exists( $class_name ) ) {
+		require_once $file;
+		$class_name::register();
+	}
+}
 
-    /**
-     * Sets plugin member variables.
-     *
-     * @since 1.0.0
-     *
-     * @ignore
-     */
-    function __construct() {
-      $this->plugin_title = plugin_basename( __FILE__ );
-      $this->plugin_path = plugin_dir_path( __FILE__ );
-    }
+/* Register Public Functionality */
+foreach ( glob( PLUGIN_PATH . '/src/public/class-*.php' ) as $file ) {
+	register_class_from_file( $file );
+}
 
-    /**
-     * Hook code into WordPress.
-     *
-     * @since 1.0.0
-     *
-     * @ignore
-     */
-    function register() {
-
-      add_action( 'admin_menu', [ $this, 'add_admin_pages' ] );
-      add_action( 'admin_enqueue_scripts', [ $this, 'register_scripts' ] );
-
-      add_action( 'add_meta_boxes', [ $this, 'add_meta_boxes' ] );
-      add_action( 'wp_ajax_refresh_page_relatives', [ $this, 'related_content_metabox_html_ajax_refresh' ] );
-
-      /* Remove nags from plugin pages */
-      add_action( 'admin_head-groups_page_ptc-grouped-content_generator', [ $this, 'remove_all_admin_notices' ], 1);
-      add_action( 'admin_head-toplevel_page_ptc-grouped-content', [ $this, 'remove_all_admin_notices' ], 1);
-
-    }
-
-    /**
-     * Add the administrative pages.
-     *
-     * @since 1.0.0
-     * @since 1.1.0 Added content generator submenu page
-     *
-     * @ignore
-     */
-    function add_admin_pages() {
-
-      add_menu_page(
-        'Grouped Content &mdash; View Groups',
-        'Groups',
-        'edit_pages',
-        'ptc-grouped-content',
-        function() {
-
-          if ( current_user_can( 'edit_pages' ) ) {
-
-            if ( isset( $_GET['post_parent'] ) ) {
-              $html_to_require = $this->plugin_path . 'view/html-group-details.php';
-            } else {
-              $html_to_require = $this->plugin_path . 'view/html-toplevel-listing.php';
-            }
-
-            require_once $html_to_require;
-
-          } else {
-            echo '<p><strong>You do not have the proper permissions to access this page.</strong></p>';
-          }
-
-        },
-        'dashicons-portfolio',
-        21 /* Pages menu item is priority 20, see https://developer.wordpress.org/reference/functions/add_menu_page/#default-bottom-of-menu-structure */
-      );
-
-      add_submenu_page(
-        'ptc-grouped-content',
-        'Grouped Content &mdash; Generator',
-        'Add New',
-        'publish_pages',
-        'ptc-grouped-content_generator',
-        function() {
-
-          if ( current_user_can( 'publish_pages' ) ) {
-            require_once $this->plugin_path . 'view/html-content-generator.php';
-          } else {
-            echo '<p><strong>You do not have the proper permissions to access this page.</strong></p>';
-          }
-
-        }
-      );
-
-    }//end add_admin_pages()
-
-    /**
-     * Add metaboxes.
-     *
-     * @since 1.0.0
-     *
-     * @ignore
-     */
-    function add_meta_boxes() {
-      add_meta_box(
-        'ptc-grouped-content',
-        'Page Relatives',
-        [ $this, 'related_content_metabox_html' ],
-        'page',
-        'side'
-      );
-    }
-
-    /**
-     * Content for the Page Relatives metabox.
-     *
-     * @since 1.0.0
-     *
-     * @ignore
-     */
-    function related_content_metabox_html() {
-      include_once $this->plugin_path . 'view/html-metabox-page-relatives.php';
-    }
-
-    /**
-     * AJAX handler for refreshing the Page Relatives metabox in Gutenberg.
-     *
-     * @since 1.2.0
-     *
-     * @ignore
-     */
-    function related_content_metabox_html_ajax_refresh() {
-      require_once $this->plugin_path . 'src/ajax-refresh-metabox-page-relatives.php';
-    }
-
-    /**
-     * Removes all admin notice actions.
-     *
-     * @since 1.2.2
-     *
-     * @ignore
-     */
-    function remove_all_admin_notices() {
-      remove_all_actions('admin_notices');
-      remove_all_actions('all_admin_notices');
-    }
-
-    /**
-     * Register and enqueue plugin CSS and JS.
-     *
-     * @since 1.0.0
-     *
-     * @ignore
-     */
-    function register_scripts( $hook_suffix ) {
-
-      wp_register_style(
-        'fontawesome',
-        'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-        [],
-        '4.7.0'
-      );
-
-      switch ( $hook_suffix ) {
-        case 'toplevel_page_ptc-grouped-content':
-          wp_enqueue_style(
-            'ptc-grouped-content_view-groups-css',
-            plugins_url( 'assets/css/view-groups.css', __FILE__ ),
-            [ 'fontawesome' ],
-            '1.0.0'
-          );
-          break;
-        case 'post.php':
-          if ( get_post_type() === 'page' ) {
-
-            wp_enqueue_style(
-              'ptc-grouped-content_metabox-page-relatives-css',
-              plugins_url( 'assets/css/metabox_page-relatives.css', __FILE__ ),
-              [ 'fontawesome' ],
-              '0.0.0'
-            );
-
-            wp_enqueue_script(
-              'ptc-grouped-content_metabox-page-relatives-js',
-              plugins_url( 'assets/js/metabox-page-relatives.js', __FILE__ ),
-              [ 'jquery' ],
-              '0.0.1'
-            );
-
-            $the_post = get_post();
-            $the_parent_post_id = -1;
-            $the_parent_post_title = '';
-            if ( is_object( $the_post ) && is_a( $the_post, '\WP_Post' ) ) {
-              if ( isset( $the_post->post_parent ) && $the_post->post_parent > 0 ) {
-                $the_parent_post_id = $the_post->post_parent;
-                $the_parent_post_title = get_the_title( $the_parent_post_id );
-              }
-            }
-
-            wp_localize_script(
-              'ptc-grouped-content_metabox-page-relatives-js',
-              'ptc_page_relatives',
-              [
-                'nonce' => wp_create_nonce( 'ptc_page_relatives' ),
-                'post_parent' => $the_parent_post_id,
-                'post_parent_title' => $the_parent_post_title,
-              ]
-            );
-
-          }
-          break;
-        case 'groups_page_ptc-grouped-content_generator':
-          wp_enqueue_style(
-            'ptc-grouped-content_content-generator-css',
-            plugins_url( 'assets/css/content-generator.css', __FILE__ ),
-            [ 'fontawesome' ],
-            '0.0.0'
-          );
-          wp_enqueue_script(
-            'ptc-grouped-content_content-generator-js',
-            plugins_url( 'assets/js/content-generator.js', __FILE__ ),
-            [ 'jquery' ],
-            '0.0.0'
-          );
-          break;
-      }
-
-    }//end register_scripts()
-
-  }//end class
-
-  $ptc_grouped_content = new PTC_Grouped_Content();
-  $ptc_grouped_content->register();
-
-}//end if class_exists
+if ( is_admin() ) {
+	/* Register Admin-Only Functionality */
+	foreach ( glob( PLUGIN_PATH . '/src/admin/class-*.php' ) as $file ) {
+		register_class_from_file( $file );
+	}
+} else {
+	/* Register Frontend-Only Functionality */
+	foreach ( glob( PLUGIN_PATH . '/src/public/frontend/class-*.php' ) as $file ) {
+		register_class_from_file( $file );
+	}
+}
---
+++ src/admin/templates/html-content-generator.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Content Generator form page
+ *
+ * A form that allows users to rapidly create content.
+ *
+ * @since 1.1.0
+ *
+ * @package PTC_Grouped_Content
+ */
+
+namespace PTC_Grouped_Content;
+
+defined( 'ABSPATH' ) || die();
+?>
+
+<header>
+	<h1>Content Generator</h1>
+</header>
+
+<div id="content-generator">
+
+	<main>
+
+		<?php require_once PLUGIN_PATH . 'src/admin/script-process-content-generator.php'; ?>
+
+		<form method="POST">
+
+			<section id="parent-page">
+
+				<h2>Parent Page</h2>
+
+				<div id="select-parent-page" class="form-group">
+					<label for="parent-page-id">Parent Page</label>
+					<select id="parent-page-id" name="parent_page_id" required>
+						<option value="-1" selected>(create new page)</option>
+						<option value="0">(no parent)</option>
+						<?php
+						$dropdown_args = [
+							'post_type'   => 'page',
+							'sort_column' => 'menu_order,post_title',
+							'post_status' => 'publish,draft,private',
+							'echo'        => false,
+						];
+						$pages_dropdown_html = wp_dropdown_pages( $dropdown_args );
+						echo preg_replace( '/<.*select.*>/i', '', $pages_dropdown_html );
+						?>
+					</select>
+				</div>
+
+				<div id="create-new-parent-page" class="form-group">
+					<label for="new-parent-page-title">Page Title</label>
+					<input id="new-parent-page-title" type="text" name="new_parent_page_title" />
+				</div>
+
+			</section>
+
+			<section id="children-pages">
+
+				<h2>Child Pages</h2>
+
+				<div class="form-group">
+					<label for="children-page-titles">Page Titles</label>
+					<textarea id="children-page-titles" name="children_page_titles" required
+						placeholder="Enter each page title on a separate line..."></textarea>
+				</div>
+
+				<div class="form-group">
+					<label for="sequential-content">Content is sequential</label>
+					<input id="sequential-content" type="checkbox" name="sequential_content" value="yes" checked>
+				</div>
+
+			</section>
+
+			<section id="menu">
+
+				<h2>Menu</h2>
+
+				<div class="form-group">
+					<label for="create-menu">Generate menu for this group</label>
+					<input id="create-menu" type="checkbox" name="create_menu" value="yes" checked>
+				</div>
+
+			</section>
+
+			<section id="content-generator-submit">
+				<input type="hidden" name="generate_content_nonce" value="<?php echo esc_attr( wp_create_nonce( 'generate_content' ) ); ?>" />
+				<input type="submit" name="generate_content" value="Generate" />
+			</section>
+
+		</form>
+
+	</main>
+
+	<aside>
+
+		<section id="help-box">
+
+			<h2><i class="fa fa-life-ring"></i>Need Help?</h2>
+
+			<div>
+
+				<p>Use the Content Generator to quickly add new draft pages to your site.</p>
+
+				<ol>
+					<li>
+						Select an existing <strong>parent page</strong>, create a new page to use as the parent page, or choose to not assign a parent page.
+					</li>
+					<li>
+						On separate lines, enter the titles of the <strong>pages you would like to create</strong>. They will all be assigned the selected parent page.
+					</li>
+					<li>
+						Choose if you would like to <strong>create a menu</strong> which links the created pages in the order you have listed them. The parent page will not be included, yet the generated menu will be named after the selected parent page, if applicable.
+					</li>
+				</ol>
+
+			</div>
+
+			<p id="contact-support">
+				<i class="fa fa-envelope"></i> If you have any questions, suggestions, or feedback, please submit your comments to the <a href="https://wordpress.org/support/plugin/grouped-content/" target="_blank">support forum</a>.
+			</p>
+
+		</section>
+
+	</aside>
+
+</div>
